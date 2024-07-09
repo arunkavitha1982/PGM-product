1b6b8166f21c838d6f79a7634d86df43cc0e0690
Arunkumar <arunkumar.msg1982@gmail.com>
2024-07-09 17:21:40 +0530

    small chnages

--- userInput.js
+++ userInput.js
@@ -1,4 +1,4 @@
-function user1() {
+function user() {
     let userName = document.querySelector(".user1")
     let userLogo = document.querySelector(".userIcon1")
     let user_star = document.querySelector(".user_star1")
@@ -50,68 +50,89 @@ function user1() {
         comments : user_comment3
     }
 
-    
-}
-
-// function user2(){
-//     let userName = document.querySelector(".user2")
-//     let userLogo = document.querySelector(".userIcon2")
-//     let user_star = document.querySelector(".user_star2")
-//     let user_comment = document.querySelector(".user_para2")
-
-//     userName.textContent = "Arunkumar";
-//     userLogo.textContent = "A"
-//     user_comment.textContent = "Pradeep from PG Mechanical was a lifesaver when our AC broke during a heatwave. He came promptly, fixed the issue, and even gave us tips on maintaining our system. Highly recommend!"
-    
-//     const user={
-//         Name : userName,
-//         logo : userLogo,
-//         star : user_star,
-//         comments : user_comment
-//     }
-// }
-
-
-// function user3(){
-//     let userName = document.querySelector(".user3")
-//     let userLogo = document.querySelector(".userIcon3")
-//     let user_star = document.querySelector(".user_star3")
-//     let user_comment = document.querySelector(".user_para3")
-
-//     userName.textContent = "Arunkumar";
-//     userLogo.textContent = "A"
-//     user_comment.textContent = "Pradeep from PG Mechanical was a lifesaver when our AC broke during a heatwave. He came promptly, fixed the issue, and even gave us tips on maintaining our system. Highly recommend!"
-    
-//     const user={
-//         Name : userName,
-//         logo : userLogo,
-//         star : user_star,
-//         comments : user_comment
-//     }
-// }
-
-function user4(){
     let userName4 = document.querySelector(".user4")
-    let userLogo = document.querySelector(".userIcon4")
-    let user_star = document.querySelector(".user_star4")
-    let user_comment = document.querySelector(".user_para4")
+    let userLogo4 = document.querySelector(".userIcon4")
+    let user_star4 = document.querySelector(".user_star4")
+    let user_comment4 = document.querySelector(".user_para4")
 
-    userName.textContent = "Arunkumar";
-    userLogo.textContent = "A"
-    user_comment.textContent = "Pradeep from PG Mechanical was a lifesaver when our AC broke during a heatwave. He came promptly, fixed the issue, and even gave us tips on maintaining our system. Highly recommend!"
+    userName4.textContent = "Arunkumar";
+    userLogo4.textContent = "A"
+    user_comment4.textContent = "Pradeep from PG Mechanical was a lifesaver when our AC broke during a heatwave. He came promptly, fixed the issue, and even gave us tips on maintaining our system. Highly recommend!"
     
-    const user={
-        Name : userName,
-        logo : userLogo,
-        star : user_star,
-        comments : user_comment
+    const user4={
+        Name : userName4,
+        logo : userLogo4,
+        star : user_star4,
+        comments : user_comment4
     }
+
+
+    let userName5 = document.querySelector(".user5")
+    let userLogo5 = document.querySelector(".userIcon5")
+    let user_star5 = document.querySelector(".user_star5")
+    let user_comment5 = document.querySelector(".user_para5")
+
+    userName5.textContent = "Arunkumar";
+    userLogo5.textContent = "A"
+    user_comment5.textContent = "Pradeep from PG Mechanical was a lifesaver when our AC broke during a heatwave. He came promptly, fixed the issue, and even gave us tips on maintaining our system. Highly recommend!"
+    
+    const user5={
+        Name : userName5,
+        logo : userLogo5,
+        star : user_star5,
+        comments : user_comment5
+    }
+
+    let userName6 = document.querySelector(".user6")
+    let userLogo6 = document.querySelector(".userIcon6")
+    let user_star6 = document.querySelector(".user_star6")
+    let user_comment6 = document.querySelector(".user_para6")
+
+    userName6.textContent = "Arunkumar";
+    userLogo6.textContent = "A"
+    user_comment6.textContent = "Pradeep from PG Mechanical was a lifesaver when our AC broke during a heatwave. He came promptly, fixed the issue, and even gave us tips on maintaining our system. Highly recommend!"
+    
+    const user6={
+        Name : userName6,
+        logo : userLogo6,
+        star : user_star6,
+        comments : user_comment6
+    }
+
+
+    let userName7 = document.querySelector(".user7")
+    let userLogo7 = document.querySelector(".userIcon7")
+    let user_star7 = document.querySelector(".user_star7")
+    let user_comment7 = document.querySelector(".user_para7")
+
+    userName6.textContent = "Arunkumar";
+    userLogo6.textContent = "A"
+    user_comment6.textContent = "Pradeep from PG Mechanical was a lifesaver when our AC broke during a heatwave. He came promptly, fixed the issue, and even gave us tips on maintaining our system. Highly recommend!"
+    
+    const user7={
+        Name : userName7,
+        logo : userLogo7,
+        star : user_star7,
+        comments : user_comment7
+    }
+
+    let userName8 = document.querySelector(".user8")
+    let userLogo8 = document.querySelector(".userIcon8")
+    let user_star8 = document.querySelector(".user_star8")
+    let user_comment8 = document.querySelector(".user_para8")
+
+    userName8.textContent = "Arunkumar";
+    userLogo8.textContent = "A"
+    user_comment8.textContent = "Pradeep from PG Mechanical was a lifesaver when our AC broke during a heatwave. He came promptly, fixed the issue, and even gave us tips on maintaining our system. Highly recommend!"
+    
+    const user8={
+        Name : userName8,
+        logo : userLogo8,
+        star : user_star8,
+        comments : user_comment8
+    }
+
+    const list=[user1,user2,user3,user4,user5,user6,user7,user8]
+    
 }
-
-
-
-
-user1();
-user2();
-user3();
-user4();
+user();
