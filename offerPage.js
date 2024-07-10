9650d88a521504803d4aaebc7a6044ceef189af7
Arunkumar <arunkumar.msg1982@gmail.com>
2024-07-11 01:44:19 +0530

    nav bar and user Testimonial changes

--- offerPage.js
+++ offerPage.js
@@ -7,7 +7,7 @@ function offer() {
         offer_title1.textContent = "AIR DUCT WHOLE HOUSE PACKAGE";
         price1.textContent = "$79";
         offer_content1.textContent = "AGITATING & AIR WASHING OF SUPPLY DUCTS, USING THE HEPA AIR METHOD";
-        expire_date1.textContent = "Offer Expires: 16/08/2023";
+        expire_date1.textContent = "Offer Expires: 12/16/2024";
 
         const offer1 = {
                 price: price1,
@@ -26,7 +26,7 @@ function offer() {
         offer_title2.textContent = "AIR DUCT WHOLE HOUSE PACKAGE";
         price2.textContent = "$89";
         offer_content2.textContent = "AGITATING & AIR WASHING OF SUPPLY DUCTS, USING THE HEPA AIR METHOD";
-        expire_date2.textContent = "Offer Expires: 28/06/2023";
+        expire_date2.textContent = "Offer Expires: 12/28/2024";
 
         const offer2={
                 price: price2,
@@ -45,7 +45,7 @@ function offer() {
         offer_title3.textContent = "NEW AIR DUCT CLEANING PACKAGE";
         price3.textContent = "$69";
         offer_content3.textContent = "ADVANCED AGITATING & AIR WASHING OF SUPPLY DUCTS, USING THE LATEST HEPA AIR METHOD";
-        expire_date3.textContent = "Offer Expires: 26/07/2023";
+        expire_date3.textContent = "Offer Expires: 12/26/2024";
 
         const offer3={
                 price: price3,
@@ -63,7 +63,7 @@ function offer() {
         offer_title4.textContent = "NEW AIR DUCT CLEANING PACKAGE";
         price4.textContent = "$69";
         offer_content4.textContent = "ADVANCED AGITATING & AIR WASHING OF SUPPLY DUCTS, USING THE LATEST HEPA AIR METHOD";
-        expire_date4.textContent = "Offer Expires: 31/08/2023";
+        expire_date4.textContent = "Offer Expires: 12/31/2024";
         const offer4={
                 price: price4,
                 head: offer_title4,
