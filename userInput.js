e8b58a16a4d8bb54c299b1e7291444ee44bdc719
Arunkumar <arunkumar.msg1982@gmail.com>
2024-07-10 16:33:13 +0530

    Update images and content

--- userInput.js
+++ userInput.js
@@ -4,8 +4,8 @@ function user() {
     let user_star = document.querySelector(".user_star1")
     let user_comment = document.querySelector(".user_para1")
 
-    userName.textContent = "Arunkumar";
-    userLogo.textContent = "A"
+    userName.textContent = "Samantha Lee";
+    userLogo.textContent = "S"
     user_comment.textContent = "Pradeep from PG Mechanical was a lifesaver when our AC broke during a heatwave. He came promptly, fixed the issue, and even gave us tips on maintaining our system. Highly recommend!"
     
     const user1={
@@ -21,9 +21,9 @@ function user() {
     let user_star2 = document.querySelector(".user_star2")
     let user_comment2 = document.querySelector(".user_para2")
 
-    userName2.textContent = "Arunkumar";
+    userName2.textContent = "Ahmed Hassan";
     userLogo2.textContent = "A"
-    user_comment2.textContent = "Pradeep from PG Mechanical was a lifesaver when our AC broke during a heatwave. He came promptly, fixed the issue, and even gave us tips on maintaining our system. Highly recommend!"
+    user_comment2.textContent = "We've been using PG Mechanical for our annual HVAC checkups for years. Pradeep and his team are always punctual, professional, and thorough. Our energy bills have never been lower!"
     
     const user2={
         Name : userName2,
@@ -39,9 +39,9 @@ function user() {
     let user_star3 = document.querySelector(".user_star3")
     let user_comment3 = document.querySelector(".user_para3")
 
-    userName3.textContent = "Arunkumar";
-    userLogo3.textContent = "A"
-    user_comment3.textContent = "Pradeep from PG Mechanical was a lifesaver when our AC broke during a heatwave. He came promptly, fixed the issue, and even gave us tips on maintaining our system. Highly recommend!"
+    userName3.textContent = "Elena Kovač";
+    userLogo3.textContent = "E"
+    user_comment3.textContent = "After struggling with an inefficient heating system, we called PG Mechanical. Pradeep suggested a new, energy-efficient model and handled the installation flawlessly. Our home has never been more comfortable."
     
     const user3={
         Name : userName3,
@@ -55,10 +55,9 @@ function user() {
     let user_star4 = document.querySelector(".user_star4")
     let user_comment4 = document.querySelector(".user_para4")
 
-    userName4.textContent = "Arunkumar";
+    userName4.textContent = " Anand Srivastav";
     userLogo4.textContent = "A"
-    user_comment4.textContent = "Pradeep from PG Mechanical was a lifesaver when our AC broke during a heatwave. He came promptly, fixed the issue, and even gave us tips on maintaining our system. Highly recommend!"
-    
+    user_comment4.textContent = "As new homeowners, we were clueless about HVAC maintenance. Pradeep from PG Mechanical took the time to educate us and set up a preventive maintenance plan. We're grateful for their patient, informative service."
     const user4={
         Name : userName4,
         logo : userLogo4,
@@ -72,10 +71,9 @@ function user() {
     let user_star5 = document.querySelector(".user_star5")
     let user_comment5 = document.querySelector(".user_para5")
 
-    userName5.textContent = "Arunkumar";
-    userLogo5.textContent = "A"
-    user_comment5.textContent = "Pradeep from PG Mechanical was a lifesaver when our AC broke during a heatwave. He came promptly, fixed the issue, and even gave us tips on maintaining our system. Highly recommend!"
-    
+    userName5.textContent = "Hiroshi Tanaka";
+    userLogo5.textContent = "H"
+    user_comment5.textContent = "Our data center requires precise temperature control 24/7. PG Mechanical's expertise in commercial HVAC has been crucial to our operations. Pradeep Gosal and his team provide reliable service and cutting-edge solutions."
     const user5={
         Name : userName5,
         logo : userLogo5,
@@ -88,10 +86,9 @@ function user() {
     let user_star6 = document.querySelector(".user_star6")
     let user_comment6 = document.querySelector(".user_para6")
 
-    userName6.textContent = "Arunkumar";
-    userLogo6.textContent = "A"
-    user_comment6.textContent = "Pradeep from PG Mechanical was a lifesaver when our AC broke during a heatwave. He came promptly, fixed the issue, and even gave us tips on maintaining our system. Highly recommend!"
-    
+    userName6.textContent = "Isabella Rossi";
+    userLogo6.textContent = "I"
+    user_comment6.textContent = "PG Mechanical transformed the refrigeration system in our multi-story restaurant. Pradeep's innovative approach not only improved efficiency but also freed up valuable kitchen space. Their ongoing maintenance keeps everything running smoothly."
     const user6={
         Name : userName6,
         logo : userLogo6,
@@ -105,10 +102,9 @@ function user() {
     let user_star7 = document.querySelector(".user_star7")
     let user_comment7 = document.querySelector(".user_para7")
 
-    userName6.textContent = "Arunkumar";
-    userLogo6.textContent = "A"
-    user_comment6.textContent = "Pradeep from PG Mechanical was a lifesaver when our AC broke during a heatwave. He came promptly, fixed the issue, and even gave us tips on maintaining our system. Highly recommend!"
-    
+    userName7.textContent = "Vikram";
+    userLogo7.textContent = "V"
+    user_comment7.textContent = "As new homeowners, we were clueless about HVAC maintenance. Pradeep from PG Mechanical took the time to educate us and set up a preventive maintenance plan. We're grateful for their patient, informative service."
     const user7={
         Name : userName7,
         logo : userLogo7,
